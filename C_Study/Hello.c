bf9247d2f592888ef553f61b09f1e36208e3b87b
s <myae3080@naver.com>
2021-03-06 08:58:32 +0900

    3

--- C_Study/Hello.c
+++ C_Study/Hello.c
@@ -1,9 +1,16 @@
-//#define _CRT_SECURE_NO_WARNINGS
+/*
+	func_s gurantees safety but, if other compiler not support this function
+	it might cause problem in code compatability.
+*/
+#define _CRT_SECURE_NO_WARNINGS
 
 #include <stdio.h>
 #include <string.h>
 
 int main(void) {
+	int num;
+	short shNum;
+	char name[20];
 	char animal[20] = "monkey";
 
 	printf("%s\n", animal);
@@ -12,10 +19,22 @@ int main(void) {
 		Without using 'define _CRT_SECURE_NO_WARNINGS',
 		it throw error because not safe to use.
 	*/
-	//strcpy(animal, "lion");
-	strcpy_s(animal, sizeof(animal), "lion");
+	strcpy(animal, "lion");
+	//strcpy_s(animal, sizeof(animal), "lion");
 
 	printf("%s\n", animal);
+
+	//scanf_s("%d", &num);
+	scanf("%d", &num);
+	// Using %d cause severe error.
+	//scanf_s("%hd", &shNum);
+	scanf("%hd", &shNum);
+
+	scanf("%s", name);
+
+	printf("입력 받은 값 : %d\n", num);
+	printf("입력 받은 값 : %d\n", shNum);
+	printf("입력 받은 값 : %s\n", name);
 	
 	system("pause");
 
