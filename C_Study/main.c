5fbc44269cbac1850ed56fc465baa1ab33d7631d
s <myae3080@naver.com>
2021-03-11 06:46:54 +0900

    change file

--- C_Study/main.c
+++ C_Study/main.c
@@ -4,7 +4,7 @@ int main(void) {
 	printf("\n");
 
 	sizeOfFunc();
-	
+
 	system("pause");
 
 	return 0;
--- C_Study/test.c
+++ C_Study/test.c
@@ -1,6 +1,47 @@
+/*
+	func_s gurantees safety but, if other compiler not support this function
+	it might cause problem in code compatability.
+*/
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <stdio.h>
+#include <string.h>
+
+void dataTypeIOFunc() {
+	printf("%s\n", "dataTypeIOFunc ===================================");
+
+	int num;
+	short shNum;
+	char name[20];
+	char animal[20] = "monkey";
+
+	printf("%s\n", animal);
+
+	/*
+		Without using 'define _CRT_SECURE_NO_WARNINGS',
+		it throw error because not safe to use.
+	*/
+	strcpy(animal, "lion");
+	//strcpy_s(animal, sizeof(animal), "lion");
+
+	printf("%s\n", animal);
+
+	//scanf_s("%d", &num);
+	scanf("%d", &num);
+	// Using %d cause severe error.
+	//scanf_s("%hd", &shNum);
+	scanf("%hd", &shNum);
+
+	scanf("%s", name);
+
+	printf("입력 받은 값 : %d\n", num);
+	printf("입력 받은 값 : %d\n", shNum);
+	printf("입력 받은 값 : %s\n", name);
+}
 
 void sizeOfFunc() {
+	printf("%s\n", "sizeOfFunc ===================================");
+
 	printf("%d\n", sizeof(int));
 	printf("%d\n", sizeof(10));
 }
